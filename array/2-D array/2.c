fe8dc7e74554d6a1d8b1c0cddb88705a3255ebfe
uday <udaykumarraj2058@gmail.com>
2024-09-19 10:06:59 +0530

    add code in 2.c

--- array/2-D array/2.c
+++ array/2-D array/2.c
@@ -1,1 +1,32 @@
 // >write a program in c to input any matrix and search any particular element after that check no is available or not.
+
+#include<stdio.h>
+int main(){
+    int arr[3][3], s, i, j, f=0;
+    printf("Enter 3x3 elements:");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    printf("\nenter searching element:");
+    scanf("%d",&s);
+
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            if(arr[i][j]==s)
+            {
+                f=1;
+                break;
+            }
+        }
+    }
+    if(f==1){
+        printf("\nsearching is found ");
+    }
+    return 0;
+}
