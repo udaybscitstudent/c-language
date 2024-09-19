d67a62209d78fe3f9886519a5cf62b2be9d5b5f6
Uday kumar <udaykumarraj2058@gmail.com>
2024-09-19 05:14:21 +0000

    add code in 3.c

--- array/2-D array/3.c
+++ array/2-D array/3.c
@@ -1,1 +1,20 @@
 // Write a program in c to input any matrix after print transpose of matrix.
+#include<stdio.h>
+int main(){
+    int arr[3][3],i,j;
+    printf("\nenter 3x3 element:");
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    printf("\nTranspose of matrix:\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++){
+            printf("%d\t",arr[j][i]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
