62055da4a978081979bff85393bb0bd26ff6de0c
Uday kumar <udaykumarraj2058@gmail.com>
2024-09-19 05:23:05 +0000

    add code in 4.c

--- array/2-D array/4.c
+++ array/2-D array/4.c
@@ -1,1 +1,33 @@
 // Write a program in c to input any matrix after that print only prime element.
+
+#include<stdio.h>
+int main(){
+    int arr[3][3],i,j,k,n,c=0;
+    printf("\nenter 3x3 element:");
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+
+    printf("\nprime number\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++){
+            n=arr[i][j];
+            for(k=1;k<=n;k++)
+            {
+                if(n%k==0)
+                {
+                    c++;
+                }
+            }
+            if(c==2)
+            {
+                printf("%d\t",n);
+            }
+            c=0;
+        }
+    }
+    return 0;
+}
