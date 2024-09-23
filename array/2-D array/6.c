a3c6d3a2003480544522560aea7209fa965556b1
uday <udaykumarraj2058@gmail.com>
2024-09-23 19:30:23 +0530

    add code in 6.c

--- array/2-D array/6.c
+++ array/2-D array/6.c
@@ -1,1 +1,39 @@
 // write a program in c to input two matrix after that check both matrix are equal or not.
+#include<stdio.h> 
+void main()
+{
+    int ar[3][3],arr[3][3],i,j,flag=1;
+    printf("enter element of first array");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&ar[i][j]);
+        }
+    }
+    printf("\nenter element of second array:");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            if(ar[i][j]!=arr[i][j])
+            {
+                flag=0;
+                break;
+            }
+        }
+    }
+    if(flag==1){
+        printf("Both matrix are equal");
+    }
+    else{
+        printf("Both matrix are not equal");
+    }
+}
