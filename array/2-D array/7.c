8e0f8e6b6a93f2a00d30a158d80ec2a7e52e5f85
uday <udaykumarraj2058@gmail.com>
2024-10-01 12:10:35 +0530

    add code in 7.c and 8.c

--- array/2-D array/7.c
+++ array/2-D array/7.c
@@ -1,1 +1,43 @@
 // >write a program in c to input two matrix after that find addition.
+#include<stdio.h>
+void main()
+{   
+    int arr[3][3],arr1[3][3],i,j , sum[3][3];
+    printf("\nenter 3x3 elements of first matrix\n:");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+        scanf("%d",&arr[i][j]);
+        }
+    }
+    printf("\nenter element of second matrix:\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+        scanf("%d",&arr1[i][j]);
+        }
+    }
+
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            sum[i][j] = arr[i][j]+arr1[i][j];
+        }
+    }
+printf("sum of both matrix:\n");
+for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d\t",sum[i][j]);
+        }
+        printf("\n");
+    }
+
+
+
+
+}
--- array/2-D array/8.c
+++ array/2-D array/8.c
@@ -1,1 +1,39 @@
 // write a program in c to input two matrix after that find multiplication.
+#include<stdio.h>
+void main()
+{   
+    int arr[3][3],arr1[3][3],i,j , mult[3][3];
+    printf("\nenter 3x3 elements of first matrix\n:");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+        scanf("%d",&arr[i][j]);
+        }
+    }
+    printf("\nenter element of second matrix:\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+        scanf("%d",&arr1[i][j]);
+        }
+    }
+
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            mult[i][j] = arr[i][j]*arr1[i][j];
+        }
+    }
+printf("Multiplication of both matrix:\n");
+for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d\t",mult[i][j]);
+        }
+        printf("\n");
+    }
+}
