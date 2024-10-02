a18c0295fc6b0e9c9ac6ecb8a146864afc003db8
uday <udaykumarraj2058@gmail.com>
2024-10-02 15:57:37 +0530

    addcode in multi-D array

--- array/multi-D array/M1.c
+++ array/multi-D array/M1.c
@@ -1,1 +1,29 @@
-// write a program in to input 5 matrix after that print all matrix.
+// write a program in c to input 5 matrix after that print all matrix.
+#include<stdio.h>
+void main(){
+    int arr[5][3][3],i,j,k;
+    for (i=0;i<5;i++)
+    {
+        printf("\nenter 3x3 value of #%d array\n",i+1);
+        for(j=0;j<3;j++)
+        {
+            for(k=0;k<3;k++)
+            {
+                scanf("%d",&arr[i][j][k]);
+            }
+        }
+    }
+    for (i=0;i<5;i++)
+    {
+        printf("\nvalue of #%d array\n",i+1);
+        for(j=0;j<3;j++)
+        {
+            for(k=0;k<3;k++)
+            {
+                printf("%d\t",arr[i][j][k]);
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
+}
--- array/multi-D array/M2.c
+++ array/multi-D array/M2.c
@@ -1,1 +1,37 @@
 // Write a program in c to input five matrix after that find sum of elements for each matrix.
+
+#include<stdio.h>
+void main()
+{
+    int arr[5][3][3],i,j,k,sum[3][3]={0};
+    for (i=0;i<5;i++)
+    {
+        printf("\nenter 3x3 value of #%d array\n",i+1);
+        for(j=0;j<3;j++)
+        {
+            for(k=0;k<3;k++)
+            {
+                scanf("%d",&arr[i][j][k]);
+            }
+        }
+    }
+    for (i=0;i<5;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            for(k=0;k<3;k++)
+            {
+                sum[j][k] = sum[j][k]+arr[i][j][k];
+            }
+        }
+    }
+    printf("\nsum of all matrix\n");
+        for(j=0;j<3;j++)
+        {
+            for(k=0;k<3;k++)
+            {
+                printf("%d\t",sum[j][k]);
+            }
+            printf("\n");
+        }
+}
